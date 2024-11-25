c9c1538ce7053c3eabcd1131f62d06bf41912a4e
cgroth06 <christopher.m.groth@gmail.com>
2024-11-25 10:28:48 -0600

    edit cleandb

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,6 +3,7 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
+    if (models[modelName].db.db) {
     let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
     }).toArray()
@@ -10,6 +11,7 @@ export default async (modelName: "Question", collectionName: string) => {
     if (modelExists.length) {
       await db.dropCollection(collectionName);
     }
+  }
   } catch (err) {
     throw err;
   }
